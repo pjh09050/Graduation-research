5ab2511be4a8d0986ca632ecc1f2b33e104077bd
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-05-22 22:48:27 +0900

    update

--- final/main.py
+++ final/main.py
@@ -16,6 +16,26 @@ if 'SUMO_HOME' in os.environ:
 else:
     sys.exit("please declare environment variable 'SUMO_HOME'")
 
+def calculate_efficiency_index():
+    # 대기 시간 추출
+    lane_ids = traci.lane.getIDList()
+    waiting_times = [traci.lane.getWaitingTime(lane_id) for lane_id in lane_ids]
+    average_waiting_time = sum(waiting_times) / len(waiting_times)
+
+    # 대기 차량 비율 추출
+    waiting_vehicle_count = sum([traci.lane.getLastStepHaltingNumber(lane_id) for lane_id in lane_ids])
+    total_vehicle_count = sum([traci.lane.getLastStepVehicleNumber(lane_id) for lane_id in lane_ids])
+    waiting_vehicle_ratio = waiting_vehicle_count / total_vehicle_count
+
+    # 통과 차량 비율 추출
+    passing_vehicle_count = total_vehicle_count - waiting_vehicle_count
+    passing_vehicle_ratio = passing_vehicle_count / total_vehicle_count
+
+    # 효율성 지수 계산
+    scaled_waiting_time = average_waiting_time / max(waiting_times)
+    efficiency_index = passing_vehicle_ratio / (scaled_waiting_time + waiting_vehicle_ratio)
+
+    return efficiency_index
 
 def get_options():
     optParser = optparse.OptionParser()
@@ -29,6 +49,9 @@ def run():
     max_step = 3600
     while step < max_step+1:
         traci.simulationStep()
+        if step > 100 and step % 10 == 0:
+            efficiency_index = calculate_efficiency_index()
+            print("Efficiency Index: {:.2f}".format(efficiency_index))
         #min_expected_number = traci.simulation.getMinExpectedNumber()
         #print(f"Step {step}: Min Expected Number: {min_expected_number}")
         step += 1
---
+++ final/performance.py
@@ -0,0 +1,33 @@
+import traci
+
+def calculate_efficiency_index():
+    # 대기 시간 추출
+    lane_ids = traci.lane.getIDList()
+    waiting_times = [traci.lane.getWaitingTime(lane_id) for lane_id in lane_ids]
+    average_waiting_time = sum(waiting_times) / len(waiting_times)
+
+    # 대기 차량 비율 추출
+    waiting_vehicle_count = sum([traci.lane.getLastStepHaltingNumber(lane_id) for lane_id in lane_ids])
+    total_vehicle_count = sum([traci.lane.getLastStepVehicleNumber(lane_id) for lane_id in lane_ids])
+    waiting_vehicle_ratio = waiting_vehicle_count / total_vehicle_count
+
+    # 통과 차량 비율 추출
+    passing_vehicle_count = total_vehicle_count - waiting_vehicle_count
+    passing_vehicle_ratio = passing_vehicle_count / total_vehicle_count
+
+    # 효율성 지수 계산
+    scaled_waiting_time = average_waiting_time / max(waiting_times)
+    efficiency_index = passing_vehicle_ratio / (scaled_waiting_time + waiting_vehicle_ratio)
+
+    return efficiency_index
+
+# SUMO 초기화 및 교통 시뮬레이션 실행
+traci.start(['sumo', '-c', 'path/to/your/sumo_config_file.sumocfg'])
+traci.simulationStep()
+
+# 효율성 지수 계산
+efficiency_index = calculate_efficiency_index()
+print("Efficiency Index:", efficiency_index)
+
+# SUMO 종료
+traci.close()
