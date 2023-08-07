295360c0d4e302942dabcf08ad09c870b0d3efe8
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-08-07 22:13:50 +0900

    update

---
+++ 3_hour_2/PSO_int.py
@@ -0,0 +1,221 @@
+import random
+from math import *
+import sys
+
+class Particle:
+    def __init__(self, bounds, max_iter):
+        self.position = []  # particle current position
+        self.velocity = []  # particle current velocity
+        self.best_position = [] # particle best position
+        self.fitness = sys.maxsize   # particle fitness
+        self.best_fitness = sys.maxsize  # particle best fitness
+        self.iteration = 0 # 반복 횟수
+        self.max_iter = max_iter
+        for i in range(len(bounds)):
+            self.position.append(int(round(random.uniform(bounds[i][0], bounds[i][1]))))
+            self.velocity.append(random.uniform(-1, 1))
+        # x1_sum = sum(self.position[:5])
+        # x2_sum = sum(self.position[5:10])
+        # x3_sum = sum(self.position[10:])
+        # print('초기해', self.position, x1_sum, x2_sum, x3_sum, sum(self.position))
+        while True:
+            x1_sum = int(round(sum(self.position[:5])))
+            x2_sum = int(round(sum(self.position[5:10])))
+            x3_sum = int(round(sum(self.position[10:])))
+            if x1_sum == 165 and x2_sum == 165 and x3_sum == 165:
+                break
+
+            if x1_sum > 165:
+                sort_list = sorted(self.position[:5], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[:5]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i])
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[:5], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[:5]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i])
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+            if x2_sum > 165:
+                sort_list = sorted(self.position[5:10], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[5:10]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i], 5, 10)
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[5:10], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[5:10]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i], 5, 10)
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+            if x3_sum > 165:
+                sort_list = sorted(self.position[10:], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[10:]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i], 10)
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[10:], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[10:]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i], 10)
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+        # x1_sum = sum(self.position[:5])
+        # x2_sum = sum(self.position[5:10])
+        # x3_sum = sum(self.position[10:])
+        # print('수정된 초기해', self.position, x1_sum, x2_sum, x3_sum, sum(self.position))
+
+    def evaluate_fitness(self, fitness_func):
+        # current position에 대한 fitness 계산
+        self.fitness = fitness_func(self.position)
+        # best_position과 best_fitness 업데이트
+        if self.fitness < self.best_fitness:
+            self.best_position = self.position
+            self.best_fitness = self.fitness
+
+    def update_velocity(self, global_best_position):
+        w_min = 0.5
+        w_max = 1
+        self.iteration += 1
+        w = w_max - ((w_max - w_min) * self.iteration / self.max_iter) # w가 점점 감소
+        c1 = 1  # 자신의 최고 위치에 대한 가중치
+        c2 = 2  # 집단의 최고 위치에 대한 가중치
+        threshold = random.random()  # 임의의 threshold 값
+        for i in range(len(self.position)):
+            r1 = random.random() # 0,1 사이의 난수
+            r2 = random.random()
+            cognitive_velocity = c1 * r1 * (self.best_position[i] - self.position[i])
+            social_velocity = c2 * r2 * (global_best_position[i] - self.position[i])
+            self.velocity[i] = w * self.velocity[i] + cognitive_velocity + social_velocity
+            # threshold를 기준으로 반올림 또는 반내림
+            if threshold > 0.3:
+                self.velocity[i] = int(round(self.velocity[i]))
+            else:
+                self.velocity[i] = int(self.velocity[i])
+        # self.velocity = [int(round(v)) for v in self.velocity]
+
+    def update_position(self, bounds):
+        for i in range(len(self.position)):
+            self.position[i] = self.position[i] + self.velocity[i]
+            self.position[i] = round(self.position[i], 1)
+            # adjust maximum position 
+            if self.position[i] < bounds[i][0]:
+                self.position[i] = bounds[i][0]
+            # adjust minimum position 
+            elif self.position[i] > bounds[i][1]:
+                self.position[i] = bounds[i][1]
+
+        # x1_sum = sum(self.position[:5])
+        # x2_sum = sum(self.position[5:10])
+        # x3_sum = sum(self.position[10:])
+        # print('update 전 position', self.position, x1_sum, x2_sum, x3_sum, sum(self.position))
+        while True:
+            x1_sum = int(round(sum(self.position[:5])))
+            x2_sum = int(round(sum(self.position[5:10])))
+            x3_sum = int(round(sum(self.position[10:])))
+            if x1_sum == 165 and x2_sum == 165 and x3_sum == 165:
+                break
+
+            if x1_sum > 165:
+                sort_list = sorted(self.position[:5], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[:5]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i])
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[:5], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[:5]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i])
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+            if x2_sum > 165:
+                sort_list = sorted(self.position[5:10], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[5:10]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i], 5, 10)
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[5:10], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[5:10]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i], 5, 10)
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+            if x3_sum > 165:
+                sort_list = sorted(self.position[10:], reverse=True)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[10:]))) == 165:
+                        break
+                    largest_value_idx = self.position.index(sort_list[i], 10)
+                    self.position[largest_value_idx] -= 1
+                    self.position[largest_value_idx] = int(round(self.position[largest_value_idx]))
+            else:
+                sort_list = sorted(self.position[10:], reverse=False)
+                for i in range(len(sort_list)):
+                    if int(round(sum(self.position[10:]))) == 165:
+                        break
+                    smallest_value_idx = self.position.index(sort_list[i], 10)
+                    self.position[smallest_value_idx] += 1
+                    self.position[smallest_value_idx] = int(round(self.position[smallest_value_idx]))
+
+        # x1_sum = sum(self.position[:5])
+        # x2_sum = sum(self.position[5:10])
+        # x3_sum = sum(self.position[10:])
+        # print('update 후 position', self.position, x1_sum, x2_sum, x3_sum, sum(self.position))
+
+class PSO:
+    def __init__(self, fitness_function, bounds, num_particles, max_iter):
+        self.fitness_func = fitness_function
+        self.bounds = bounds
+        self.num_particles = num_particles
+        self.max_iter = max_iter
+        self.global_best_position = []  # group best position
+        self.global_best_fitness = -1  # group best fitness
+        self.swarm = [] # 초기 particles
+        for i in range(num_particles):
+            self.swarm.append(Particle(bounds, self.max_iter))
+            
+    def run_result(self):
+        for i in range(self.max_iter):
+            # 각 particle의 fitness 평가하기
+            for j in range(self.num_particles):
+                self.swarm[j].evaluate_fitness(self.fitness_func)
+                # global_best_position, global_best_fitness 업데이트
+                if self.swarm[j].fitness < self.global_best_fitness or self.global_best_fitness == -1:
+                    self.global_best_position = list(self.swarm[j].position)
+                    self.global_best_fitness = float(self.swarm[j].fitness)
+            # 각 particle의 position과 velocity 업데이트
+            for j in range(self.num_particles):
+                self.swarm[j].update_velocity(self.global_best_position)
+                self.swarm[j].update_position(self.bounds)
+        print('Best position:', self.global_best_position)
+        print('sum best position', sum(self.global_best_position))
+        print('Best fitness:', self.global_best_fitness)
+        return self.global_best_position, self.global_best_fitness
+
+
---
+++ 3_hour_2/Traffic_data.py
@@ -0,0 +1,284 @@
+import numpy as np
+import random
+
+# W1_car = 1800
+# N1_car = 455
+# S1_car = 358
+# N2_car = 128
+# S2_car = 153
+# N3_car = 1147
+# S3_car = 1881
+# E3_car = 720
+# time = 3600
+# W1_car = 3600
+# N1_car = 910
+# S1_car = 716
+# N2_car = 256
+# S2_car = 306
+# N3_car = 2294
+# S3_car = 3982
+# E3_car = 1440
+# time = 7200
+W1_car = 5400
+N1_car = 1365
+S1_car = 1074
+N2_car = 384
+S2_car = 459
+N3_car = 3441
+S3_car = 5863
+E3_car = 2160
+time = 10800
+
+def total_traffic():
+    np.random.seed(1234)
+    total_list = []
+    sorted_list = []
+    W1_arrival_rate = W1_car / time  # 차량 도착율 계산
+    W1_time_list = [] # W1 등장 차량 시간 리스트
+    W1_arrival_intervals = np.random.exponential(scale=1/W1_arrival_rate, size=W1_car) # scale은 지수 분포의 평균 역수를 나타내는 값
+    W1_cumulative_interval = 0
+    for i, interval in enumerate(W1_arrival_intervals):
+        # W1출발부터 모든 경로에 대한 확률 가중치 부여 후 경로 선택
+        car_direction = random.choices(population=["routeW1_N1", "routeW1_S1", "routeW1_N2", "routeW1_S2", "routeW1_S3", "routeW1_E3"], weights=[0.1,0.1,0.08,0.05,0.07,0.6], k=1)[0]
+        W1_cumulative_interval += interval
+        if W1_cumulative_interval > time:
+            break
+        if car_direction == "routeW1_N1":
+            departLane = 3
+            arrivalLane = 2
+        elif car_direction == "routeW1_S1":
+            departLane= 0
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeW1_N2":
+            departLane = random.randint(1,2)
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeW1_S2":
+            departLane = random.randint(1,2)
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeW1_S3":
+            departLane = random.randint(1,2)
+            arrivalLane = 0
+        elif car_direction == "routeW1_E3":
+            a = random.random()
+            if a < 0.9:
+                departLane = random.randint(1,2)
+            else:
+                departLane = random.randint(0,2)
+            arrivalLane = random.randint(1,3)
+        W1_time_list.append((car_direction, np.round(W1_cumulative_interval,1), departLane, arrivalLane))
+
+    N1_arrival_rate = N1_car / time  # 차량 도착율 계산
+    N1_time_list = [] # N1 차량 등장 시간 리스트
+    N1_arrival_intervals = np.random.exponential(scale=1/N1_arrival_rate, size=N1_car)
+    N1_cumulative_interval = 0
+    for i, interval in enumerate(N1_arrival_intervals):
+        car_direction = random.choices(population=["routeN1_W1", "routeN1_S1", "routeN1_S2", "routeN1_S3", "routeN1_E3"], weights=[0.1, 0.608, 0.015, 0.027, 0.25], k=1)[0]
+        N1_cumulative_interval += interval
+        if N1_cumulative_interval > time:
+            break
+        if car_direction == "routeN1_W1":
+            departLane = 0
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeN1_S1":
+            a = random.random()
+            if a < 0.9:
+                departLane = random.randint(1,2)
+                arrivalLane = departLane
+            else:
+                departLane = random.randint(0,2)
+                arrivalLane = departLane
+        elif car_direction == "routeN1_S2":
+            departLane = 3
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeN1_S3":
+            departLane = 3
+            arrivalLane = 0
+        elif car_direction == "routeN1_E3":
+            departLane = 3
+            arrivalLane = random.randint(2,3)
+        N1_time_list.append((car_direction, np.round(N1_cumulative_interval,1), departLane, arrivalLane))
+
+    S1_arrival_rate = S1_car / time  # 차량 도착율 계산
+    S1_time_list = []
+    S1_arrival_intervals = np.random.exponential(scale=1/S1_arrival_rate, size=S1_car)
+    S1_cumulative_interval = 0
+    for i, interval in enumerate(S1_arrival_intervals):
+        car_direction = random.choices(population=["routeS1_W1", "routeS1_N1", "routeS1_S3", "routeS1_E3"], weights=[0.1, 0.623, 0.027, 0.25], k=1)[0]
+        S1_cumulative_interval += interval
+        if S1_cumulative_interval > time:
+            break
+        if car_direction == "routeS1_W1":
+            departLane = 3
+            arrivalLane = 2
+        elif car_direction == "routeS1_N1":
+            a = random.random()
+            if a < 0.9:
+                departLane = random.randint(1,2)
+                arrivalLane = departLane
+            else:
+                departLane = random.randint(0,1)
+                arrivalLane = departLane
+        elif car_direction == "routeS1_S3":
+            departLane = 0
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeS1_E3":
+            departLane = 0
+            arrivalLane = random.randint(1,2)
+        S1_time_list.append((car_direction, np.round(S1_cumulative_interval,1), departLane, arrivalLane))
+
+    N2_arrival_rate = N2_car / time  # 차량 도착율 계산
+    N2_time_list = []
+    N2_arrival_intervals = np.random.exponential(scale=1/N2_arrival_rate, size=N2_car)
+    N2_cumulative_interval = 0
+    for i, interval in enumerate(N2_arrival_intervals):
+        car_direction = random.choices(population=["routeN2_W1", "routeN2_S1", "routeN2_S2", "routeN2_S3", "routeN2_E3"], weights=[0.07, 0.02, 0.5, 0.04, 0.37], k=1)[0]
+        N2_cumulative_interval += interval
+        if N2_cumulative_interval > time:
+            break
+        if car_direction == "routeN2_W1":
+            departLane = 0
+            arrivalLane = 0
+        elif car_direction == "routeN2_S1":
+            departLane= 0
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeN2_S2":
+            a = random.random()
+            if a < 0.6:
+                departLane = 1
+                arrivalLane = 1
+            else:
+                departLane = 0
+                arrivalLane = 0
+        elif car_direction == "routeN2_S3":
+            departLane = 1
+            arrivalLane = 0
+        elif car_direction == "routeN2_E3":
+            departLane = 1
+            arrivalLane = random.randint(2,3)
+        N2_time_list.append((car_direction, np.round(N2_cumulative_interval,1), departLane, arrivalLane))
+
+    S2_arrival_rate = S2_car / time  # 차량 도착율 계산
+    S2_time_list = []
+    S2_arrival_intervals = np.random.exponential(scale=1/S2_arrival_rate, size=S2_car)
+    S2_cumulative_interval = 0
+    for i, interval in enumerate(S2_arrival_intervals):
+        car_direction = random.choices(population=["routeS2_W1", "routeS2_N1", "routeS2_N2", "routeS2_S3", "routeS2_E3"], weights=[0.09, 0.01, 0.5, 0.04, 0.36], k=1)[0]
+        S2_cumulative_interval += interval
+        if S2_cumulative_interval > time:
+            break
+        if car_direction == "routeS2_W1":
+            departLane = 1
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeS2_N1":
+            departLane= 1
+            arrivalLane = 0
+        elif car_direction == "routeS2_N2":
+            a = random.random()
+            if a < 0.6:
+                departLane = 1
+                arrivalLane = 1
+            else:
+                departLane = 0
+                arrivalLane = 0
+        elif car_direction == "routeS2_S3":
+            departLane = 0
+            arrivalLane = 0
+        elif car_direction == "routeS2_E3":
+            departLane = 0
+            arrivalLane = random.randint(1,2)
+        S2_time_list.append((car_direction, np.round(S2_cumulative_interval,1), departLane, arrivalLane))
+
+    N3_arrival_rate = N3_car / time  # 차량 도착율 계산
+    N3_time_list = []
+    N3_arrival_intervals = np.random.exponential(scale=1/N3_arrival_rate, size=N3_car)
+    N3_cumulative_interval = 0
+    for i, interval in enumerate(N3_arrival_intervals):
+        car_direction = random.choices(population=["routeN3_W1", "routeN3_N1", "routeN3_S1", "routeN3_S2", "routeN3_S3"], weights=[0.036, 0.009, 0.01, 0.01, 0.8], k=1)[0]
+        N3_cumulative_interval += interval
+        if N3_cumulative_interval > time:
+            break
+        if car_direction == "routeN3_W1":
+            departLane = 0
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeN3_N1":
+            departLane= 0
+            arrivalLane = 0
+        elif car_direction == "routeN3_S1":
+            departLane = 0
+            arrivalLane = 2
+        elif car_direction == "routeN3_S2":
+            departLane = 0
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeN3_S3":
+            departLane = random.randint(1,3)
+            arrivalLane = departLane
+        N3_time_list.append((car_direction, np.round(N3_cumulative_interval,1), departLane, arrivalLane))
+
+    S3_arrival_rate = S3_car / time  # 차량 도착율 계산
+    S3_time_list = []
+    S3_arrival_intervals = np.random.exponential(scale=1/S3_arrival_rate, size=S3_car)
+    S3_cumulative_interval = 0
+    for i, interval in enumerate(S3_arrival_intervals):
+        car_direction = random.choices(population=["routeS3_W1", "routeS3_N1", "routeS3_S1", "routeS3_N2", "routeS3_N3", "routeS3_E3"], weights=[0.02, 0.003, 0.004, 0.003, 0.87, 0.1], k=1)[0]
+        S3_cumulative_interval += interval
+        if S3_cumulative_interval > time:
+            break
+        if car_direction == "routeS3_W1":
+            departLane = 4
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeS3_N1":
+            departLane= 4
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeS3_S1":
+            departLane = 4
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeS3_N2":
+            departLane = 4
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeS3_N3":
+            departLane = random.randint(1,3)
+            arrivalLane = departLane
+        elif car_direction == "routeS3_E3":
+            departLane = 0
+            arrivalLane = 1
+        S3_time_list.append((car_direction, np.round(S3_cumulative_interval,1), departLane, arrivalLane))
+    
+    E3_arrival_rate = E3_car / time  # 차량 도착율 계산
+    E3_time_list = []
+    E3_arrival_intervals = np.random.exponential(scale=1/E3_arrival_rate, size=E3_car)
+    E3_cumulative_interval = 0
+    for i, interval in enumerate(E3_arrival_intervals):
+        car_direction = random.choices(population=["routeE3_W1", "routeE3_N1", "routeE3_S1", "routeE3_N2", "routeE3_S2", "routeE3_N3", "routeE3_S3"], weights=[0.37, 0.04, 0.08, 0.06, 0.05, 0.1, 0.3], k=1)[0]
+        E3_cumulative_interval += interval
+        if E3_cumulative_interval > time:
+            break
+        if car_direction == "routeE3_W1":
+            a = random.random()
+            if a < 0.9:
+                departLane = random.randint(1,2)
+            else:
+                departLane = random.randint(0,2)
+            arrivalLane = random.randint(0,2) 
+        elif car_direction == "routeE3_N1":
+            departLane= random.randint(0,2)
+            arrivalLane = 0
+        elif car_direction == "routeE3_S1":
+            departLane = random.randint(1,2)
+            arrivalLane = random.randint(1,2)
+        elif car_direction == "routeE3_N2":
+            departLane = 0
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeE3_S2":
+            departLane = 2
+            arrivalLane = random.randint(0,1)
+        elif car_direction == "routeE3_N3":
+            departLane = 0
+            arrivalLane = 0
+        elif car_direction == "routeE3_S3":
+            departLane = 3
+            arrivalLane = random.randint(2,3)
+        E3_time_list.append((car_direction, np.round(E3_cumulative_interval,1), departLane, arrivalLane))
+
+    total_list = W1_time_list + N1_time_list + S1_time_list + N2_time_list + S2_time_list + N3_time_list + S3_time_list + E3_time_list
+    sorted_list = sorted(total_list, key=lambda x: x[1])
+    return sorted_list
