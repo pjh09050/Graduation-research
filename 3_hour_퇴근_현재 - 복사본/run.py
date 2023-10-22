50c3b678ac3510f02b0357d6856bd0ea858ac579
Park Jinhwan <106535998+pjh09050@users.noreply.github.com>
2023-10-22 21:43:14 +0900

    update

--- 3_hour_퇴근_현재 - 복사본/run.py
+++ 3_hour_퇴근_현재 - 복사본/run.py
@@ -99,9 +99,9 @@ def main():
     # current_phases2 = [60, 3, 22, 3, 18, 4, 3, 61, 3, 3]
 
     # 3시간 PSO
-    # current_phases3 = [26, 3, 15, 3, 25, 3, 48, 3, 51, 3]
-    # current_phases4 = [26, 3, 22, 3, 17, 3, 42, 3, 58, 3]
-    # current_phases5 = [56, 3, 15, 3, 21, 8, 3, 65, 3, 3]
+    current_phases3 = [26, 3, 15, 3, 25, 3, 48, 3, 51, 3]
+    current_phases4 = [26, 3, 22, 3, 17, 3, 42, 3, 58, 3]
+    current_phases5 = [56, 3, 15, 3, 21, 8, 3, 65, 3, 3]
                         
     # 3시간 PSO_정규화
     # current_phases6 = [27, 3, 16, 3, 23, 3, 53, 3, 46, 3]
@@ -109,9 +109,9 @@ def main():
     # current_phases8 = [56, 3, 19, 3, 19, 6, 3, 65, 3, 3]
     
     # 3시간 Discrete_PSO
-    current_phases9 = [25, 3, 16, 3, 22, 3, 40, 3, 62, 3]
-    current_phases10 = [25, 3, 20, 3, 27, 3, 41, 3, 52, 3] 
-    current_phases11 = [56, 3, 19, 3, 21, 4, 3, 65, 3, 3]
+    # current_phases9 = [25, 3, 16, 3, 22, 3, 40, 3, 62, 3]
+    # current_phases10 = [25, 3, 20, 3, 27, 3, 41, 3, 52, 3] 
+    # current_phases11 = [56, 3, 19, 3, 21, 4, 3, 65, 3, 3]
 
     options = False
     if options == False:
@@ -120,39 +120,39 @@ def main():
         sumoBinary = checkBinary('sumo-gui')
 #####################################################################################################################################################################################
 
-    waiting_result = []
-    left_right_result = []
-    left_right_max_waiting_result = []
-    right_left_result = []
-    right_left_max_waiting_result = []
-    up_down_result = []
-    down_up_result = []
-    # travel_result = []
-    data_list = []
+    # waiting_result = []
+    # left_right_result = []
+    # left_right_max_waiting_result = []
+    # right_left_result = []
+    # right_left_max_waiting_result = []
+    # up_down_result = []
+    # down_up_result = []
+    # # travel_result = []
+    # data_list = []
     
-    while run_step < 100:
-        print('{}번째 기존 신호 시뮬레이션'.format(run_step+1))
-        generate_routefile() # 교통량 생성
+    # while run_step < 100:
+    #     print('{}번째 기존 신호 시뮬레이션'.format(run_step+1))
+    #     generate_routefile() # 교통량 생성
 
-        # traci를 사용하여 sumo와 python을 연결
-        traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end","--start", "--no-warnings"])
-        # traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end", "--no-warnings"])
-        # sumo에서 신호 세팅해주는 부분
-        current_phases0, current_phases1, current_phases2 = modify_phase(current_phases0, current_phases1, current_phases2)
+    #     # traci를 사용하여 sumo와 python을 연결
+    #     traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end","--start", "--no-warnings"])
+    #     # traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end", "--no-warnings"])
+    #     # sumo에서 신호 세팅해주는 부분
+    #     current_phases0, current_phases1, current_phases2 = modify_phase(current_phases0, current_phases1, current_phases2)
 
-        # sumo 시뮬레이션  성능 추출하는 부분
-        average_waiting_time_list, all_direction_average, left_right_list_average, max_waiting_time, right_left_list_average, left_right_waiting_time, up_down_list_average, down_up_list_average = run()
+    #     # sumo 시뮬레이션  성능 추출하는 부분
+    #     average_waiting_time_list, all_direction_average, left_right_list_average, max_waiting_time, right_left_list_average, left_right_waiting_time, up_down_list_average, down_up_list_average = run()
 
-        waiting_result.append(all_direction_average)
-        left_right_result.append(left_right_list_average)
-        # left_right_max_waiting_result.append(max_waiting_time)
-        right_left_result.append(right_left_list_average)
-        # right_left_max_waiting_result.append(left_right_waiting_time)
-        up_down_result.append(up_down_list_average)
-        down_up_result.append(down_up_list_average)
-        # travel_result.append(average_travel_time)
-        data_list.append(average_waiting_time_list)
-        run_step += 1
+    #     waiting_result.append(all_direction_average)
+    #     left_right_result.append(left_right_list_average)
+    #     # left_right_max_waiting_result.append(max_waiting_time)
+    #     right_left_result.append(right_left_list_average)
+    #     # right_left_max_waiting_result.append(left_right_waiting_time)
+    #     up_down_result.append(up_down_list_average)
+    #     down_up_result.append(down_up_list_average)
+    #     # travel_result.append(average_travel_time)
+    #     data_list.append(average_waiting_time_list)
+    #     run_step += 1
 #####################################################################################################################################################################################
 
     # run_step = 0
@@ -244,7 +244,7 @@ def main():
         traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end","--start", "--no-warnings"])
         # traci.start([sumoBinary, "-c", "new1.sumocfg", "--tripinfo-output", "tripinfo.xml", "--quit-on-end", "--no-warnings"])
         # sumo에서 신호 세팅해주는 부분
-        current_phases9, current_phases10, current_phases11 = modify_phase(current_phases9, current_phases10, current_phases11)
+        current_phases3, current_phases4, current_phases5 = modify_phase(current_phases3, current_phases4, current_phases5)
 
         # sumo 시뮬레이션  성능 추출하는 부분
         average_waiting_time_list3, all_direction_average3, left_right_list_average3, max_waiting_time3, right_left_list_average3, left_right_waiting_time3, up_down_list_average3, down_up_list_average3 = run()
@@ -327,35 +327,35 @@ def main():
 
     # df_current = pd.DataFrame([average_waiting_time_list])
     # df_pso = pd.DataFrame([average_waiting_time_list3])
-    df_waiting_result = pd.DataFrame(waiting_result)
+    # df_waiting_result = pd.DataFrame(waiting_result)
     df_waiting_result1 = pd.DataFrame(waiting_result3)
-    df_waiting_result.to_csv('waiting_result 결과.csv')
-    df_waiting_result1.to_csv('waiting_result3 결과.csv')
+    # df_waiting_result.to_csv('waiting_result 결과.csv')
+    df_waiting_result1.to_csv('waiting_result1 결과.csv')
 
-    df_left_right_result = pd.DataFrame(left_right_result)
+    # df_left_right_result = pd.DataFrame(left_right_result)
     df_left_right_result1 = pd.DataFrame(left_right_result3)
-    df_left_right_result.to_csv('left_right_result 결과.csv')
-    df_left_right_result1.to_csv('left_right_result3 결과.csv')
+    # df_left_right_result.to_csv('left_right_result 결과.csv')
+    df_left_right_result1.to_csv('left_right_result1 결과.csv')
 
-    df_right_left_result = pd.DataFrame(right_left_result)
+    # df_right_left_result = pd.DataFrame(right_left_result)
     df_right_left_result1 = pd.DataFrame(right_left_result3)
-    df_right_left_result.to_csv('right_left_result 결과.csv')
-    df_right_left_result1.to_csv('right_left_result3 결과.csv')
+    # df_right_left_result.to_csv('right_left_result 결과.csv')
+    df_right_left_result1.to_csv('right_left_result1 결과.csv')
 
-    df_up_down_result = pd.DataFrame(up_down_result)
+    # df_up_down_result = pd.DataFrame(up_down_result)
     df_up_down_result1 = pd.DataFrame(up_down_result3)
-    df_up_down_result.to_csv('up_down_result 결과.csv')
-    df_up_down_result1.to_csv('up_down_result3 결과.csv')
+    # df_up_down_result.to_csv('up_down_result 결과.csv')
+    df_up_down_result1.to_csv('up_down_result1 결과.csv')
 
-    df_down_up_result = pd.DataFrame(down_up_result)
+    # df_down_up_result = pd.DataFrame(down_up_result)
     df_down_up_result1 = pd.DataFrame(down_up_result3)
-    df_down_up_result.to_csv('down_up_result 결과.csv')
-    df_down_up_result1.to_csv('down_up_result3 결과.csv')
+    # df_down_up_result.to_csv('down_up_result 결과.csv')
+    df_down_up_result1.to_csv('down_up_result1 결과.csv')
 
-    df_current = pd.DataFrame(data_list)
+    # df_current = pd.DataFrame(data_list)
     df_pso = pd.DataFrame(data_list3)
-    df_current.to_csv('기존 신호 시뮬레이션 결과.csv')
-    df_pso.to_csv('PSO 3 신호 시뮬레이션 결과.csv')
+    # df_current.to_csv('기존 신호 시뮬레이션 결과.csv')
+    df_pso.to_csv('PSO 1 신호 시뮬레이션 결과.csv')
     
 if __name__ == "__main__":
     main()
